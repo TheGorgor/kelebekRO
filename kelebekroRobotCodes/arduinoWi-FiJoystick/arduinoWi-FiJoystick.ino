ffe52c3ddd92c6074fa4bef15cc7481c287757f9
TheGorgor <garen.ezikoglu1@gmail.com>
2025-05-02 16:04:24 +0300

    Add files via upload

---
+++ kelebekroRobotCodes/arduinoWi-FiJoystick/arduinoWi-FiJoystick.ino
@@ -0,0 +1,35 @@
+#include "SPI.h" 
+#include "RF24.h" 
+#include "nRF24L01.h" 
+
+RF24 radio(9, 8);
+const byte addres[6] = "GETRO";
+
+byte motorValues[2] = {};
+
+void setup() {
+ 
+  pinMode(A3, INPUT);
+  //VRy - 1
+ 
+  pinMode(A5, INPUT);
+  //VRy - 2
+
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+
+}
+
+void loop() {
+  
+  motorValues[0] = map(analogRead(A3);, 0, 1023, 0, 255);
+  motorValues[1] = map(analogRead(A5);, 0, 1023, 0, 255);
+
+  radio.write(&motorValues, sizeof(motorValues));
+
+  delay(100);
+
+}
